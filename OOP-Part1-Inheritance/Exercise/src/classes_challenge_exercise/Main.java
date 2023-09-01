1077097438155dfd720bda8994d5316c732c40f5
Agility6 <agility1013@gmail.com>
2023-09-02 01:52:36 +0800

    chore: file rename

---
+++ OOP-Part1-Inheritance/Exercise/src/classes_challenge_exercise/Main.java
@@ -0,0 +1,32 @@
+package classes_challenge_exercise;
+
+/**
+ * ClassName: Main
+ * Description:
+ *
+ * @Author Agility6
+ * @Create 2023-08-21
+ * @Version 1.0
+ */
+public class Main {
+  public static void main(String[] args) {
+    // Test
+    Account bobsAccount = new Account();
+    bobsAccount.setNumber("123456");
+    bobsAccount.setBalance(1000.00);
+    bobsAccount.setCustomerName("Ag");
+    bobsAccount.setCustomerEmail("Ag@ag.com");
+    bobsAccount.setCustomerPhone("135xxxxxxxx");
+
+  //  bobsAccount.withdrawFunds(100.0);
+  //  bobsAccount.depositFunds(130.0);
+  //  bobsAccount.withdrawFunds(30);
+  //  bobsAccount.withdrawFunds(100);
+
+  //  bobsAccount.depositFunds(100);
+  //  bobsAccount.withdrawFunds(55.5);
+  //  bobsAccount.withdrawFunds(60.5);
+  //  bobsAccount.withdrawFunds(44.5);
+
+  }
+}
---
+++ OOP-Part1-Inheritance/Exercise/src/constructors_Challenge/Customer.java
@@ -0,0 +1,45 @@
+package constructors_Challenge;
+
+public class Customer {
+  private String name;
+  private String limit;
+  private String email;
+
+  public Customer() {
+  }
+
+  public Customer(String name, String limit, String email) {
+    this.name = name;
+    this.limit = limit;
+    this.email = email;
+  }
+
+  public Customer(String name, String email) {
+    this.name = name;
+    this.email = email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLimit() {
+    return limit;
+  }
+
+  public void setLimit(String limit) {
+    this.limit = limit;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
